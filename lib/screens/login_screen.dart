23b30960eb7f25d5b379a08d900312f2fb5ad924
= <=>
2026-09-10 19:26:06 -0600

    feat:(login):add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscure = true;
   @override
   Widget build(BuildContext context) {
     //Para obtener el tamaño de la pantalla
@@ -24,6 +25,45 @@ class _LoginScreenState extends State<LoginScreen> {
               height:200,
               child: RiveAnimation.asset('login-bear.riv')
             ),
+             //para separar espacio
+             SizedBox(height: 10),
+             TextField(
+              //para mostrar el tipo de teclado
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                prefixIcon:const Icon(Icons.email),
+                border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12)
+                )
+              ),
+             ),
+             SizedBox(height: 10),
+             //campo de texto para contraseña
+             TextField(
+              obscureText: _obscure,
+              //Para mostrar el tipo de teclado
+               keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                prefixIcon:const Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: (){
+                    //refrescar el icono
+                    setState((){
+                      _obscure = !_obscure;
+                    });
+                  },
+                ),
+                border: OutlineInputBorder(
+                  //para redondear bordes
+                borderRadius: BorderRadius.circular(12),
+                )
+              ),
+             ),
             ],
           ),
           ),
